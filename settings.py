bc5085aec53101bdd424c7cf1bf8bd6cfaa0e195
Pasha <157066314+MegaFullStackProgramer@users.noreply.github.com>
2024-01-28 17:18:38 +0200

    A Sample for your alien game
    Made with Pyhton and some popular frameworks, just need to add enemies and your game is done!

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings():
+    def __init__(self):
+        #Window
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(6,0,73)
+        #Ship
+        self.ship_speed_factor=1
+
+
+
+        #Bullet
+        self.bullet_speed_factor=1
+        self.bullet_width=3
+        self.bullet_height=15
+        self.bullet_color=(96,96,96)
